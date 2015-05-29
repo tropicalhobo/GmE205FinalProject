ee743cf603db26ad67734c9d8903756a3d7d6899
tropicalhobo <jose.torres@obf.ateneo.edu>
2015-05-29 21:22:36 +0800

    new file:   CONVERT_TORASTER.py 	modified:   TRANSFORM_NDBI.py

---
+++ CONVERT_TORASTER.py
@@ -0,0 +1,5 @@
+import ogr, gdal
+from gdalconst import *
+import os, sys
+import time as tm
+import numpy as np
--- TRANSFORM_NDBI.py
+++ TRANSFORM_NDBI.py
@@ -1,17 +1,28 @@
-import os
 import gdal
 from gdalconst import *
 import numpy as np
-import time
+import os, sys. time
 from math import sqrt
 
-def transformNDBI(wrksp):
-    """Opens raw .TIF images and applies radiometric rescale constants."""
+def setcwd():
+    """Sets current working directory of the script."""
+    cwd = os.getcwd()
+    return cwd
+
+def modifyName(nom):
+    """Returns modified input file name string."""
+    fn = nom.strip().split('CMSK_REFLECTANCE.TIF')
+    return fn[0] + '_NDBI.TIF'
+
+def transformNDBI(cwd):
+    """Transform Landat dataset to NDBI."""
    
     band4 = None
     band5 = None
     fName = ''
-    for f in wrksp:
+    workspace = os.listdir(cwd)
+    
+    for f in workspace:
         
         if 'B4' in f: 
             band4 = gdal.Open(f, GA_ReadOnly)
@@ -19,14 +30,14 @@ def transformNDBI(wrksp):
             print f
         elif 'B5' in f:
             band5 = gdal.Open(f, GA_ReadOnly)
-            print f 
+            print f
+        else:
+            print 'Cannot find images.'
+            sys.exit(1)
            
-    b4cols = band4.RasterXSize
-    b4rows = band4.RasterYSize
-    b5cols = band5.RasterXSize
-    b5rows = band5.RasterYSize
-    geotrans = band5.GetGeoTransform()
-    projection = band5.GetProjection()
+    b4cols, b4rows = band4.RasterXSize, band4.RasterYSize
+    b5cols, b5rows = band5.RasterXSize, band5.RasterYSize
+    geotrans, proj = band5.GetGeoTransform(), band5.GetProjection()
     driver = band5.GetDriver()
         
     data4 = band4.ReadAsArray(0,0,b4cols,b4rows).astype('float32')
@@ -35,31 +46,28 @@ def transformNDBI(wrksp):
     ndbi = (data5-data4)/(data4+data5)
      
     outData = driver.Create(modifyName(fName),b5cols,b5rows,1, GDT_Float32)
-    outData.SetProjection(projection)
+    outData.SetProjection(proj)
     outData.SetGeoTransform(geotrans)
     oBand = outData.GetRasterBand(1)
     oBand.WriteArray(ndbi)
     oBand.FlushCache()
-        
+
+    #Memory management    
     band4 = None
     band5 = None
     
     data4 = None
     data5 = None
+
+    ndbi = None
     
     outData = None
-
-def modifyName(nom):
-    """Returns modified input file name string."""
-    fn = nom.strip().split('_')
-    fn[1] = '_NDBI.TIF'
-    return fn[0]+fn[1]
 
 def main():
     start = time.time()
 
-    workSpace = os.listdir('C:\\Users\G Torres\\Desktop\\GEOG 213 DATA PROCESSING\\1988081')
-    transformNDBI(workSpace)
+    cwd = setcwd()
+    transformNDBI(cwd)
     
     print time.time()-start, 'seconds'
     
