8d8af50da1b54e6f792357c06096ec8859094aab
tropicalhobo <jose.torres@obf.ateneo.edu>
2015-05-29 20:38:04 +0800

    modified:   CALIBRATE_RADIANCE.py 	modified:   TEST1_MAIN.py

--- TEST1_MAIN.py
+++ TEST1_MAIN.py
@@ -1,17 +1,21 @@
 import gdal
 from gdalconst import *
 import os
-import time
+import time as tm
 import sys
 import MASK_CLOUD as mc
 import CALIBRATE_RADIANCE as cr
 
 def main():
+    start = tm.time()
+    
     workdir = mc.setcwd()
     fmaskout = mc.findFmask(workdir)
     mc.cloudmask(fmaskout,workdir)
     #calibrate
     cr.calibrateRadiance(workdir)
+
+    print 'Script process took %f seconds' % (tm.time()-start)
     
 if __name__=='__main__':
     main()
